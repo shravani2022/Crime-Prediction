1b64d7078a3bc0562b23fccb2421ac67d9f15be5
AMIT KAMBLE <shravanishedage22@gmail.com>
2025-04-26 00:13:20 +0530

    Initial commit

---
+++ database.sql
@@ -0,0 +1,34 @@
+/*
+SQLyog Enterprise - MySQL GUI v6.56
+MySQL - 5.5.5-10.1.13-MariaDB : Database - crime_hotspot
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`crime_hotspot` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `crime_hotspot`;
+
+/*Table structure for table `user_registration` */
+
+DROP TABLE IF EXISTS `user_registration`;
+
+CREATE TABLE `user_registration` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `Password` varchar(100) DEFAULT NULL,
+  `Phone` varchar(100) DEFAULT NULL,
+  `Address` varchar(100) DEFAULT NULL,
+  `DOB` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
